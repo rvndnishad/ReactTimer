9479aa729579b415453dac2ab496928c5ed80e8f
Arvind Nishad <rvndnishad@gmail.com>
2016-11-04 04:52:38 +0530

    Add Timer and Countdown components

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,8 +8,8 @@ var Nav = () => {
           <div className="top-bar-left">
               <ul className="menu">
                 <li className="menu-text">React Timer</li>
-                <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</IndexLink></li>
-                <li><Link to="/Countdown" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Countdown</Link></li>
+                <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
+                <li><Link to="/Countdown" activeClassName="active-link">Countdown</Link></li>
               </ul>
           </div>
           <div className="top-bar-right">
